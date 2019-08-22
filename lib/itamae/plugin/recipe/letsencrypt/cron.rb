38fe1df59240b2ef17a367de6cc7733975758000
Manabu Seki <github@zitaku.net>
2019-08-22 20:12:11 +0900

    Restart HTTP server when renewing certificate

--- lib/itamae/plugin/recipe/letsencrypt/cron.rb
+++ lib/itamae/plugin/recipe/letsencrypt/cron.rb
@@ -1,8 +1,16 @@
-cron_text = <<-EOS
-# DO NOT EDIT
-# BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
-0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew
-EOS
+if node[:letsencrypt][:authenticator] == 'webroot' && node[:letsencrypt][:http_server] != nil
+  cron_text = <<-EOS
+  # DO NOT EDIT
+  # BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
+  0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew && /bin/systemctl reload #{node[:letsencrypt][:http_server]}
+  EOS
+else
+  cron_text = <<-EOS
+  # DO NOT EDIT
+  # BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
+  0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew
+  EOS
+end
 
 file node[:letsencrypt][:cron_file_path] do
   content cron_text
