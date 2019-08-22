1dba8ee21e10fc1e25c0dd1e7ddb6ba949eb7f34
Manabu Seki <github@zitaku.net>
2019-08-22 20:27:54 +0900

    Mod Indent

--- lib/itamae/plugin/recipe/letsencrypt/cron.rb
+++ lib/itamae/plugin/recipe/letsencrypt/cron.rb
@@ -1,15 +1,15 @@
 if node[:letsencrypt][:authenticator] == 'webroot' && node[:letsencrypt][:http_server] != nil
   cron_text = <<-EOS
-  # DO NOT EDIT
-  # BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
-  0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew && /bin/systemctl reload #{node[:letsencrypt][:http_server]}
-  EOS
+# DO NOT EDIT
+# BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
+0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew && /bin/systemctl reload #{node[:letsencrypt][:http_server]}
+EOS
 else
   cron_text = <<-EOS
-  # DO NOT EDIT
-  # BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
-  0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew
-  EOS
+# DO NOT EDIT
+# BECAUSE THIS CRON CREATE BY itamae-plugin-recipe-letsencrypt
+0 0 1 * * #{node[:letsencrypt][:cron_user]} #{node[:letsencrypt][:certbot_auto_path]} renew
+EOS
 end
 
 file node[:letsencrypt][:cron_file_path] do
